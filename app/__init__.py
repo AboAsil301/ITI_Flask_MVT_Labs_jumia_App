4802d8bdc2ddb6892d43d3f90671fbe462956120
AboAsil301 <aboasil9459@gmail.com>
2023-10-23 06:13:22 +0300

    add database configration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 # import flask
-# load configuration
-
 from flask import Flask
 
+# load configuration
+from app.config import projectConfig as AppConfig
 
-def create_app():
+# get db object
+from app.models import  db
+
+
+def create_app(config_name='dev'):
     app = Flask(__name__)
+    # object of config class(DevelopmentConfig or ProductionConfig)from config.py base on config_name
+    current_App_Config = AppConfig[config_name]
+    # load configuration from current_App_Config object
+    app.config.from_object(current_App_Config)
+    # initialize db
+    db.init_app(app)
     return app
--- app/config.py
+++ app/config.py
@@ -5,12 +5,12 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    DEBUG=True
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI= "sqlite:///project.sqlite"
 
 
 class ProductionConfig(Config):
-    DEBUG=False
+    DEBUG = False
     # postgresql://username:password@localhost:portnumber/dbname
     SQLALCHEMY_DATABASE_URI= "postgresql://flask:207301@localhost:5432/jumia"
 
